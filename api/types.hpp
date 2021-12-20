bd5ddad0cecb6867f89a6b2031798be8f0a0254d
New-generation-hsc <hsc199761@163.com>
2021-12-20 19:21:28 +0800

    feat: support edge weight preprocess

--- api/types.hpp
+++ api/types.hpp
@@ -10,8 +10,9 @@ typedef uint32_t rank_t;  /* block rank */
 typedef uint16_t hid_t;   /* walk hop */
 typedef uint16_t tid_t;   /* thread id */
 typedef uint32_t wid_t;   /* walk id */
+typedef float    real_t;  /* edge weight */
 
-struct walk_t { 
+struct walk_t {
     hid_t hop   : 16;
     vid_t pos    : 24;   /* current walk current pos vertex */
     vid_t source : 24;   /* walk source vertex */
